65132b3497c1b5551109ccfc56845c9d06d46c7f
Joshua Cruz <150260152+crimsonfall@users.noreply.github.com>
2023-11-22 18:45:52 -0600

    Set Password.java to use com.app.concert.user package

--- src/com/app/concert/user/Password.java
+++ src/com/app/concert/user/Password.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package concertdb;
+package com.app.concert.user;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
