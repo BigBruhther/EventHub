dbf2b162c125a56117d04a9c8196e33940491c0b
Joshua Cruz <150260152+crimsonfall@users.noreply.github.com>
2023-11-23 23:38:59 -0600

    Added GlobalData.java

---
+++ src/utils/GlobalData.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import bo.User;
+import javax.swing.JDesktopPane;
+
+/**
+ *
+ * @author Joshua Cruz
+ */
+public class GlobalData {
+     public static User usr = null;
+}
