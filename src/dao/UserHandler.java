a1178e81ebdb51505d5b1908936b58148d1d31ba
Joshua Cruz <150260152+crimsonfall@users.noreply.github.com>
2023-11-23 23:39:36 -0600

    Added UserHandler.java

---
+++ src/dao/UserHandler.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import utils.SQLUtil;
+
+/**
+ *
+ * @author ayylmao
+ */
+public class UserHandler {
+    private SQLUtil sqlUtil;
+    public UserHandler() {
+        sqlUtil = new SQLUtil();
+    }
+    
+    public int addUser(String email, String password, int iD){
+        String cmdTemplate = "insert into User(email, password, iD) values('%s', '%s', '%d')";
+        String cmd = String.format(cmdTemplate, email, password, iD);
+        return sqlUtil.execeuteUpdate(cmd);
+    }
+};
