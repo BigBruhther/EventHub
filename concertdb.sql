ea5d18af3ff568408a18a492b51f40a055f97aff
Joshua Cruz <150260152+crimsonfall@users.noreply.github.com>
2023-11-26 19:01:57 -0600

    Added SQL file (incomplete)

---
+++ concertdb.sql
@@ -0,0 +1,32 @@
+create database concertdb;
+use concertdb;
+
+create table User(
+	userID int primary key auto_increment,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null
+);
+
+create table Artist(
+	artistID int primary key,
+    name varchar(255) not null,
+    genre varchar(255) not null
+);
+
+create table Concert(
+	name varchar(255) primary key,
+    cDate date,
+    price int
+);
+
+create table Venue(
+	vId int primary key,
+    address varchar(255),
+    vName varchar(255)
+);
+
+-- Add more SQL code here or alter as needed.
+
+select * from User;
+select userID, name from User where email='qqq' and password='ppp'
